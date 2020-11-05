ab88a0fb8e6623e5b29038a34a970b98f59fc110
Kai Wen <kaiwen_lum@mymail.sutd.edu.sg>
2020-11-05 19:51:42 +0800

    updated GUI

--- private/kai/panthera_gui_control/control.js
+++ private/kai/panthera_gui_control/control.js
@@ -1,4 +1,4 @@
-/ Connecting to ROS
+// Connecting to ROS
   // -----------------
 
   var ros = new ROSLIB.Ros({
@@ -35,12 +35,51 @@
   let rb_cw  =  document.getElementById("rb_cw ");
   let rb_acw =  document.getElementById("rb_acw");
 
+  let battery =  document.getElementById("battery");
+
+  let temp_health = document.getElementById("temp_health");
+  let temp_value = document.getElementById("temp_value");
+
+  let volt_health = document.getElementById("volt_health");
+  let volt_value = document.getElementById("volt_value");
+
+  let current_health = document.getElementById("current_health");
+  let current_value = document.getElementById("current_value");
+
+  var svg_lf_text = document.getElementById("svg_lf_text").textContent;
+  
 
   var target_angle = new ROSLIB.Topic({
     ros: ros,
     name: '/target_angle',
     messageType: 'geometry_msgs/Twist'
 
+  });
+
+  var encoder_positions = new ROSLIB.Topic({
+    ros: ros,
+    name: '/encoder_positions',
+    messageType: 'geometry_msgs/Twist'
+
+  });
+
+  var encoder_speed = new ROSLIB.Topic({
+    ros: ros,
+    name: '/encoder_speed',
+    messageType: 'geometry_msgs/Twist'
+
+  });
+
+  var battery_info = new ROSLIB.Topic({
+    ros: ros,
+    name: '/bms_data',
+    messageType: 'battery_info_pub.battery_info'
+  });
+
+  var battery_twist = new ROSLIB.Topic({
+    ros: ros,
+    name : "/battery_twist",
+    messageType: "geometry_msgs/Twist"
   });
 
 
@@ -56,7 +95,89 @@
     name : '/recalibrate_encoder',
     messageType : 'std_msgs/Int32'
   });
+
+  var cnt = 0;
+
   
+
+  function update_batt_data(data){
+    var T = data.linear.x;
+    var V = data.linear.y;
+    var I = data.linear.z;
+
+    Plotly.extendTraces("temp",{y:[[T]]}, [0]);
+    Plotly.extendTraces("volt",{y:[[V]]}, [0]);
+    Plotly.extendTraces("current",{y:[[I]]}, [0]);
+
+    var T_percent = T/360;
+
+
+    temp_health.value = (T/360) *100;
+    temp_value.innerHTML="Temperature:"+  T.toString() +"K";
+
+    volt_health.value = (V/360) *100;
+    volt_value.innerHTML="Voltage:"+  V.toString() +"V";
+
+    current_health.value = (I/360) *100;
+    current_value.innerHTML="Current:"+  I.toString() +"A";
+
+    
+
+
+    if(cnt > 30){
+      Plotly.relayout("temp", {
+        xaxis: {
+          range: [cnt-30,cnt]
+        }
+        });
+      };
+  
+      if(cnt > 30){
+        Plotly.relayout("volt", {
+          xaxis: {
+            range: [cnt-30,cnt]
+          }
+          });
+        };
+
+        if(cnt > 30){
+          Plotly.relayout("current", {
+            xaxis: {
+              range: [cnt-30,cnt]
+            }
+            });
+          };
+
+          cnt++
+  
+  };
+
+  function update_angle(data){
+    var lf = data.linear.x;
+    var rf= data.linear.y;
+    var lb= data.linear.z;
+    var rf = data.angular.x
+
+    document.getElementById("svg_lf_text").textContent = lf;
+    document.getElementById("svg_rf_text").textContent = rf;
+    document.getElementById("svg_lb_text").textContent = lb;
+    document.getElementById("svg_rb_text").textContent = rf;
+
+    document.getElementById("svg_lf_path").style.strokeDasharray = lf;
+    document.getElementById("svg_rf_path").style.strokeDasharray = rf;
+    document.getElementById("svg_lb_path").style.strokeDasharray = lb;
+    document.getElementById("svg_rb_path").style.strokeDasharray = rf;
+
+
+
+
+  }
+
+  battery_info.subscribe(function(msg){console.log("batt info sub")});
+  battery_twist.subscribe(function(msg){update_batt_data(msg)})
+  encoder_positions.subscribe(function(msg){update_angle(msg)})
+
+
 
   submitSpeed.onclick = function(){
     speed_lf    = document.getElementById("lf_trans").value;
@@ -80,21 +201,24 @@
 
     target_speed.publish(twist_speed);
   };
-
+/*
   pubRecalib.onclick  = function(){
     encoder_idx = document.getElementById("encoder_idx");
     recalib_encoder.publish(encoder_idx);
   };
   
-
+*/
 
   submitAngle.onclick = function(){
-    angle_lf  = document.getElementById("lf_rot").value;
-    angle_lb  = document.getElementById("lb_rot").value;
-    angle_rf  = document.getElementById("rb_rot").value;
-    angle_rb  = document.getElementById("rf_rot").value;
+    angle_lf  = parseFloat(document.getElementById("lf_rot").value);
+    angle_lb  = parseFloat(document.getElementById("lb_rot").value);
+    angle_rf  = parseFloat(document.getElementById("rb_rot").value);
+    angle_rb  = parseFloat(document.getElementById("rf_rot").value);
     alert("sending target angle" );
-    console.log(text);
+    console.log(angle_lf);
+    console.log(angle_lb);
+    console.log(angle_rf);
+    console.log(angle_rb);
 
     var twist_angle = new ROSLIB.Message({
         linear : {
@@ -104,45 +228,61 @@
         },
         angular : {
           x : angle_rb,
-          y : 0,
-          z : 0
+          y : 0.0,
+          z : 0.0,
         }
       });
 
-    target_angle.publish(newText);
+    target_angle.publish(twist_angle);
   };
 
-    lf_cw.onclick   = function{
+
+  Plotly.plot("temp",[{
+    y:[0],
+    type:"line"
+    }]);
+
+  Plotly.plot("volt",[{
+      y:[0],
+      type:"line"
+      }]);
+    
+  Plotly.plot("current",[{
+      y:[0],
+      type:"line"
+      }]);
+
+
+
+  
+  
+    /*   
+    lf_cw.onclick   = function(){
         //modbus set speed
-    }
-    lf_acw.onclick  = function{
+    };
+    lf_acw.onclick  = function(){
         //modbus set speed
-    }
-    lb_cw.onclick   = function{
+    };
+    lb_cw.onclick   = function(){
         //modbus set speed
-    }
-    lb_acw.onclick  = function{
+    };
+    lb_acw.onclick  = function(){
         //modbus set speed
-    }
-    rf_cw.onclick   = function{
+    };
+    rf_cw.onclick   = function(){
         //modbus set speed
-    }
-    rf_acw.onclick  = function{
+    };
+    rf_acw.onclick  = function(){
         //modbus set speed
-    }
-    rb_cw.onclick   = function{
+    };
+    rb_cw.onclick   = function(){
         //modbus set speed
-    }
-    rb_acw.onclick  = function{
+    };
+    rb_acw.onclick  = function(){
         //modbus set speed
-    }
+    };
 
   // Subscribing to command velocity
 
- 
+*/
 
-
-   
-  cmd_vel.subscribe(function(msg){print_vel(msg)});
-
-  
